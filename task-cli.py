776e9a687abbdd3a6e7af697654eab48680bf72d
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-11-13 17:33:03 -0500

    Implement task addition and persistence with JSON file storage
    - Added functionality to create and store tasks in a JSON file (`all_tasks.json`).
    - If the file does not exist, it is created and the task is added.
    - If the file exists, the new task is appended to the existing list of tasks.
    - Task details are stored as a dictionary with a task description.
    - Success message is displayed after a task is added.

--- task-cli.py
+++ task-cli.py
@@ -1,5 +1,6 @@
 import argparse
-
+import json
+import os
 # Create a main parser
 main_parsers = argparse.ArgumentParser(description="Task Tracker using CLI")
 
@@ -13,3 +14,29 @@ add_task_parser.add_argument('task_name', help="Name of the tasks")
 # Parse the argument
 args = main_parsers.parse_args()
 
+if args.command == 'add':
+	# Create a task:
+	task = {
+		'description': args.task_name
+	}
+	# Store all the tasks in the list
+	tasks_list = [task]
+	# Create a file
+	fileName = 'all_tasks.json'
+	if not os.path.exists(fileName):
+		# Create a new JSON file and add the tasks_list in it
+		with open(fileName, 'w') as file:
+			json.dump(tasks_list, file, indent=4)
+	else:
+		# Read the file
+		with open(fileName, 'r') as file:
+			tasks_list_json = json.load(file)
+
+		# Append the task in it.
+		tasks_list_json.append(task)
+
+		# Update the file using the new task list
+		with open(fileName, 'w') as file:
+			json.dump(tasks_list_json, file, indent=4)
+
+	print(f"Output: Task added successfully: {tasks_list}")
