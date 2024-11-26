b1c427bdc50f35bf68fb3cc87d68124b7ba9de72
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-11-25 19:05:42 -0500

    fix: Handle file existence and empty content scenarios
    - Added checks to handle cases where `all_tasks.json` does not exist or is empty for all commands.
    - Improved user feedback for commands like `list`, `delete`, `update`, `mark-in-progress`, and `mark-done`.

--- task-cli.py
+++ task-cli.py
@@ -15,7 +15,8 @@ add_task_parser.add_argument('add_task_name', help="Name of the tasks")
 
 # Create "list" task command
 list_task_parser = subparsers.add_parser('list', help="Listing all tasks")
-list_task_parser.add_argument('list_status', choices=['todo', 'in-progress', 'done'], nargs='?', help='Listing by status')
+list_task_parser.add_argument('list_status', choices=['todo', 'in-progress', 'done'], nargs='?',
+                              help='Listing by status')
 
 # Create "delete" task command
 delete_task_parser = subparsers.add_parser('delete', help="Delete a task")
@@ -73,84 +74,119 @@ if args.command == 'add':
 
 # Check if the command is 'list'
 if args.command == 'list':
-	# Read the JSON file and converted to python list
-	with open(fileName, 'r') as file:
-		tasks_lists = json.load(file)
-	print(f"{'Id':<5}{'Description':<20}{'Status':^10}{'Created At':>25}")
-	# check if the list choice set to: to-do
-	if args.list_status == 'todo':
-		for task in tasks_lists:
-			if args.list_status == task['status']:  # Only display to-do tasks
-				print(f"{task['id']:<5}{task['description']:<20}{task['status']:^10}{task['createAt']:>30}")
-
-	# Check if the list choice set to: in-progress
-	if args.list_status == 'in-progress':
-		for task in tasks_lists:
-			if args.list_status == task['status']: # Only display tasks that are in-progress
-				print(f"{task['id']:<5}{task['description']:<20}{task['status']:^10}{task['createAt']:>30}")
-
-	# Check if the list choice set to: done
-	if args.list_status == 'done':
-		for task in tasks_lists:
-			if args.list_status == task['status']:
-				print(f"{task['id']:<5}{task['description']:<20}{task['status']:^10}{task['createAt']:>30}")
+	# if the file does not exit
+	if not os.path.exists(fileName):
+		print("The file does not exist. Create a new task")
+	# if there is no content in the existing file
+	elif os.path.getsize(fileName) == 0:
+		print("There is no task in the file. Create a new task")
 	else:
-		for task in tasks_lists:  # Display all the tasks
-			print(f"{task['id']:<5}{task['description']:<20}{task['status']:^10}{task['createAt']:>30}")
+		# Read the JSON file and converted to python list
+		with open(fileName, 'r') as file:
+			tasks_lists = json.load(file)
+		print(f"{'Id':<5}{'Description':<20}{'Status':^10}{'Created At':>25}")
+		# check if the list choice set to: to-do
+		if args.list_status == 'todo':
+			for task in tasks_lists:
+				if args.list_status == task['status']:  # Only display to-do tasks
+					print(f"{task['id']:<5}{task['description']:<20}{task['status']:^10}{task['createAt']:>30}")
+
+		# Check if the list choice set to: in-progress
+		if args.list_status == 'in-progress':
+			for task in tasks_lists:
+				if args.list_status == task['status']:  # Only display tasks that are in-progress
+					print(f"{task['id']:<5}{task['description']:<20}{task['status']:^10}{task['createAt']:>30}")
+
+		# Check if the list choice set to: done
+		if args.list_status == 'done':
+			for task in tasks_lists:
+				if args.list_status == task['status']:
+					print(f"{task['id']:<5}{task['description']:<20}{task['status']:^10}{task['createAt']:>30}")
+		else:
+			for task in tasks_lists:  # Display all the tasks
+				print(f"{task['id']:<5}{task['description']:<20}{task['status']:^10}{task['createAt']:>30}")
 
 # Check if the command is 'delete'
 if args.command == 'delete':
-	# Read the JSON file
-	with open(fileName, 'r') as file:
-		tasks_lists = json.load(file)
-	# Traverse the tasks_list
-	for task in tasks_lists:
-		# check if the delete id match with task id
-		if args.delete_id == task['id']:
-			tasks_lists.remove(task)  # remove the task from the list
-	# Update the JSON file using the tasks_lists
-	with open(fileName, 'w') as file:
-		json.dump(tasks_lists, file, indent=4)
+	# if the file does not exit
+	if not os.path.exists(fileName):
+		print("The file does not exist. Create a new task")
+	# if there is no content in the existing file
+	elif os.path.getsize(fileName) == 0:
+		print("There is no task in the file. Create a new task")
+	else:
+		# Read the JSON file
+		with open(fileName, 'r') as file:
+			tasks_lists = json.load(file)
+		# Traverse the tasks_list
+		for task in tasks_lists:
+			# check if the delete id match with task id
+			if args.delete_id == task['id']:
+				tasks_lists.remove(task)  # remove the task from the list
+		# Update the JSON file using the tasks_lists
+		with open(fileName, 'w') as file:
+			json.dump(tasks_lists, file, indent=4)
 
 # Check if the command is 'update'
 if args.command == 'update':
-	# Read the JSON file
-	with open(fileName, 'r') as file:
-		tasks_lists = json.load(file)  # Store converted python list in tasks_lists
-	# Traverse the tasks_lists
-	for task in tasks_lists:
-		# Check if the update id match with task id in task list
-		if args.update_id == task['id']:
-			task['description'] = args.update_task_name  # Update the task name
+	# if the file does not exit
+	if not os.path.exists(fileName):
+		print("The file does not exist. Create a new task")
+	# if there is no content in the existing file
+	elif os.path.getsize(fileName) == 0:
+		print("There is no task in the file. Create a new task")
+	else:
+		# Read the JSON file
+		with open(fileName, 'r') as file:
+			tasks_lists = json.load(file)  # Store converted python list in tasks_lists
+		# Traverse the tasks_lists
+		for task in tasks_lists:
+			# Check if the update id match with task id in task list
+			if args.update_id == task['id']:
+				task['description'] = args.update_task_name  # Update the task name
 
-	# Update the JSON file using this new tasks_lists
-	with open(fileName, 'w') as file:
-		json.dump(tasks_lists, file, indent=4)
+		# Update the JSON file using this new tasks_lists
+		with open(fileName, 'w') as file:
+			json.dump(tasks_lists, file, indent=4)
 
 # Check if the command is 'mark-in-progress'
 if args.command == 'mark-in-progress':
-	# Read the JSON file and convert it to python list
-	with open(fileName, 'r') as file:
-		tasks_lists = json.load(file)
-	# Traverse the list
-	for task in tasks_lists:
-		# Check if the mark_in_progress_id match with the task id
-		if args.mark_in_progress_id == task['id']:
-			task['status'] = 'in-progress'  # update the task's statue: in-progress
-	# Update the JSON file using the new tasks_lists
-	with open(fileName, 'w') as file:
-		json.dump(tasks_lists, file, indent=4)
+	# if the file does not exit
+	if not os.path.exists(fileName):
+		print("The file does not exist. Create a new task")
+	# if there is no content in the existing file
+	elif os.path.getsize(fileName) == 0:
+		print("There is no task in the file. Create a new task")
+	else:
+		# Read the JSON file and convert it to python list
+		with open(fileName, 'r') as file:
+			tasks_lists = json.load(file)
+		# Traverse the list
+		for task in tasks_lists:
+			# Check if the mark_in_progress_id match with the task id
+			if args.mark_in_progress_id == task['id']:
+				task['status'] = 'in-progress'  # update the task's statue: in-progress
+		# Update the JSON file using the new tasks_lists
+		with open(fileName, 'w') as file:
+			json.dump(tasks_lists, file, indent=4)
 
 # Check if the command is 'mark-done'
 if args.command == 'mark-done':
-	# Read the JSON file and convert it to Python List
-	with open(fileName, 'r') as file:
-		tasks_lists = json.load(file)
-	# Traverse the tasks_lists
-	for task in tasks_lists:
-		# check if the mark_done_id match with the tasks
-		if args.mark_done_id == task['id']:
-			task['status'] = 'done'  # Update the status to done
-	# Update the JSON file using the new tasks_lists
-	with open(fileName, 'w') as file:
-		json.dump(tasks_lists, file, indent=4)
+	# if the file does not exit
+	if not os.path.exists(fileName):
+		print("The file does not exist. Create a new task")
+	# if there is no content in the existing file
+	elif os.path.getsize(fileName) == 0:
+		print("There is no task in the file. Create a new task")
+	else:
+		# Read the JSON file and convert it to Python List
+		with open(fileName, 'r') as file:
+			tasks_lists = json.load(file)
+		# Traverse the tasks_lists
+		for task in tasks_lists:
+			# check if the mark_done_id match with the tasks
+			if args.mark_done_id == task['id']:
+				task['status'] = 'done'  # Update the status to done
+		# Update the JSON file using the new tasks_lists
+		with open(fileName, 'w') as file:
+			json.dump(tasks_lists, file, indent=4)
